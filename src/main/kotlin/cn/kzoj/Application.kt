e45897c6e1bed8fd9e6f8a68b0a7b77e77748187
YSY <shaoyuanyu@foxmail.com>
2024-01-20 00:29:09 +0800

    init

---
+++ src/main/kotlin/cn/kzoj/Application.kt
@@ -0,0 +1,14 @@
+package cn.kzoj
+
+import cn.kzoj.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureSerialization()
+    configureRouting()
+}
