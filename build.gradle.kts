ce0db7ba51571a0bef70538b2f6e6f740301934f
YSY <shaoyuanyu@foxmail.com>
2024-01-20 01:25:39 +0800

    chore:引入ktorm和mysql-connector-j

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val ktorm_version: String by project
+val mysql_connector_version: String by project
 
 plugins {
     kotlin("jvm") version "1.9.22"
@@ -23,6 +25,7 @@ repositories {
 }
 
 dependencies {
+    // ktor
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-auth-jvm")
     implementation("io.ktor:ktor-client-core-jvm")
@@ -31,7 +34,17 @@ dependencies {
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
+
+    // ktorm
+    implementation("org.ktorm:ktorm-core:$ktorm_version")
+
+    // mysql-connector-j
+    implementation("com.mysql:mysql-connector-j:$mysql_connector_version")
+
+    // logback
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    // kotlin test junit
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
