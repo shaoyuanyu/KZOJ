98f6cc851c0a2615101fded7bb01df785b766998
YSY <shaoyuanyu@foxmail.com>
2024-10-19 17:35:12 +0800

    chore:升级Ktor到3.0.0，更新依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ val datetimeVersion: String by project
 
 plugins {
     kotlin("jvm") version "2.0.20"
-    id("io.ktor.plugin") version "2.3.12"
+    id("io.ktor.plugin") version "3.0.0"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20"
 }
 
@@ -37,21 +37,21 @@ repositories {
 
 dependencies {
     // ktor-server
-    implementation("io.ktor:ktor-server-auth-jvm")
-    implementation("io.ktor:ktor-server-auth-jwt-jvm")
-    implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-server-cors-jvm")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm")
-    implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("io.ktor:ktor-server-resources-jvm")
+    implementation("io.ktor:ktor-server-auth")
+    implementation("io.ktor:ktor-server-auth-jwt")
+    implementation("io.ktor:ktor-server-core")
+    implementation("io.ktor:ktor-server-cors")
+    implementation("io.ktor:ktor-server-content-negotiation")
+    implementation("io.ktor:ktor-server-netty")
+    implementation("io.ktor:ktor-server-resources")
     implementation("io.ktor:ktor-server-status-pages")
-    testImplementation("io.ktor:ktor-server-tests-jvm")
+    testImplementation("io.ktor:ktor-server-tests-host")
 
     // ktor client
-    implementation("io.ktor:ktor-client-core-jvm")
-    implementation("io.ktor:ktor-client-apache-jvm")
-    implementation("io.ktor:ktor-client-content-negotiation-jvm")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+    implementation("io.ktor:ktor-client-core")
+    implementation("io.ktor:ktor-client-apache")
+    implementation("io.ktor:ktor-client-content-negotiation")
+    implementation("io.ktor:ktor-serialization-kotlinx-json")
 
     // exposed
     implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
