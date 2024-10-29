f670f2bdf32cb0b5c0dcaf4c733aada7df0b5810
YSY <shaoyuanyu@foxmail.com>
2024-10-29 10:37:32 +0800

    fix:为开发环境配置CORS

--- src/main/kotlin/cn/kzoj/plugins/Cors.kt
+++ src/main/kotlin/cn/kzoj/plugins/Cors.kt
@@ -5,15 +5,17 @@ import io.ktor.server.application.*
 import io.ktor.server.plugins.cors.routing.*
 
 fun Application.configureCORS() {
+    // TODO: 仅开发环境
     install(CORS) {
-        allowMethod(HttpMethod.Options)
-        allowMethod(HttpMethod.Put)
-        allowMethod(HttpMethod.Delete)
-        allowMethod(HttpMethod.Patch)
+        anyMethod()
 
         allowHeader(HttpHeaders.Authorization)
         allowHeader(HttpHeaders.ContentType)
+        allowNonSimpleContentTypes = true
 
-        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+        allowCredentials = true
+        allowSameOrigin = true
+
+        anyHost()
     }
 }
